18876b907675e6b374bf17916e8d5751fcdcd2bf
Nihad512 <73600990+Nihad512@users.noreply.github.com>
2023-02-05 19:20:21 +0100

    Update AboutMe.jsx

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -10,7 +10,18 @@ function AboutMe() {
         <span className='flex direction_column About'>
         <p>Front-End developer</p>
         <h1>Nihad Babayev</h1>
-        <p className='myInfo'>Potřebujete vytvořit webové stránky ? Při tvorbě webových stránek zajišťuji celkový design od návrhu až po realizaci. Perfektní technickou stránku zajistišťuji též, aby všechno běželo přesně jak má. Kdyby by Vás mé portofilo přípradně zkušenosti zaujaly - napište mi !</p>
+        <p className='myInfo'>
+        My name is Nihad Babayev,
+I am a self trained Frontend
+developer. I am from
+Azerbaijan but, currently I
+am living in Poland,
+Warsaw. Currently I can
+speak in Azeri(native),
+Turkish(c2), Russian(A1),
+Polish(B1) And
+English(B2). 
+        </p>
         <span className='flex direction_row'>
         <Link to="/Portfolio" className='flex justify_center align_center'>Portfolio</Link>
         <Link to="/Contact" className='flex justify_center align_center'>Contact</Link>
